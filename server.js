9a6b4c9905eeca1cc95f1f78a8dff314153d932b
Nate Jones <15nate.jones@gmail.com>
2021-08-29 21:46:02 -0400

    Fix server connection and updates to api

--- server.js
+++ server.js
@@ -1,31 +1,32 @@
+//Dependencies
 require("dotenv").config();
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-//Process.env.port is important for heroku!
+//Port declartation
 const PORT = process.env.PORT ||3000;
 
 const app = express();
-
+// Log all changes to output
 app.use(logger("dev"));
 
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+// Allows usage of static files on a server
 app.use(express.static("public"));
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+// Utilize mongoDB connection
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budgetSave", {
 useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
 })
 
-// routes
+// Routes import
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`App running on port http://localhost:${PORT}!`);
 });
